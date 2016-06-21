1b0fd550080165a574ddd9f751b86848c909a34b
malgogi <dlowgh7@gmail.com>
2016-06-21 11:40:49 +0900

    update

--- UniverseZZU/Assets/Scripts/GameController.cs
+++ UniverseZZU/Assets/Scripts/GameController.cs
@@ -16,11 +16,23 @@ public class GameController : MonoBehaviour {
 
 	//solution과 스텝을 체크하는 method
 	bool MatchCheck() {
+		//solution
+		Transform[] solution = GameObject.Find ("SolutionWrapper").transform.FindChild ("Solution").GetComponentsInChildren<Transform>();
+		foreach( Transform cube in solution ) {
+			print ( cube.gameObject.name );
+		}
+
+		Transform[] stage = GameObject.Find ("Stage").GetComponentsInChildren<Transform> ();
+		foreach( Transform cube in solution ) {
+			print ( cube.gameObject.name );
+		}
+
 		return true;
 	}
 
 	//solution을 보여주는 함수
 	public void ShowSolution() {
+		MatchCheck ();
 		GameObject.Find ( "SolutionWrapper" ).transform.FindChild( "Solution" ).gameObject.SetActive ( true );
 		GameObject.Find ( "Canvas" ).transform.FindChild( "ButtonBack" ).gameObject.SetActive ( true );
 		GameObject.Find ( "Canvas" ).transform.FindChild( "ButtonSolution" ).gameObject.SetActive (false);
