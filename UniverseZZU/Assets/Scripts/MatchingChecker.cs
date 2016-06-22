6f7da016951df51cf45829fd6c66d5a720da6e9f
wonyeong91 <wonyeong91@naver.com>
2016-06-22 12:28:36 +0900

    hi

---
+++ UniverseZZU/Assets/Scripts/MatchingChecker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchingChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find("Solid").activeSelf) {
+			print ("hi");
+		}
+	}
+}
